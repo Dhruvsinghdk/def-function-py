30131f292339a0a431cce762cbb281bc9b2e0d0b
Dhruvsinghdk <re621006@gmail.com>
2024-09-12 00:55:30 +0530

    new file

---
+++ Loop.py
@@ -0,0 +1,17 @@
+# def sum(a,b):
+#     return a+b
+
+# print("Sum = ",sum(1,2))
+
+# def mul(a,b):
+#     print("Multiply = ",a*b)
+
+# mul(2,3)
+
+
+
+def table(a):
+    for e in range(10+1):
+        print(e,"*",a,"=",e*a)
+
+table(2)
