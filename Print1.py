6b44e725ad30264e8505ce2b2f55e36f113c3f3f
Dhruvsinghdk <re621006@gmail.com>
2024-09-12 01:26:24 +0530

    new file

---
+++ Print1.py
@@ -0,0 +1,17 @@
+# list = [14,5,6,6,7,7]
+# print(len(list))
+
+
+# list = ["h","e","l","l","o"]
+
+# def line():
+#     for e in range(len(list)):
+#         print(list[e], end = "")
+
+# line()
+
+def convert(usd):
+    inr = usd * 83
+    print(inr)
+
+convert(4)
---
+++ default.py
@@ -0,0 +1,4 @@
+def add(a,b = 2):
+    print(a+b)
+add(1) 
+
